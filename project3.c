004c43702c162c90bc3cba1b1e1c18cf895d9df7
Erik Steggall <esteggal@ucsc.edu>
2011-05-25 00:27:11 -0700

    added shiftleft2x() to project3.c and .h

--- project3.c
+++ project3.c
@@ -4,8 +4,12 @@
 //
 
 
-
-int main(){
-  printf("hello world\n");
-  return 0;
+void shiftleft2x(unsigned char out[32], unsigned char in[32]){
+  int i;
+  out[0] = 0;
+  out[1] = 0;
+  for(i = 1;i < 31;i++){
+    out[i+1] = in[i];
+  }
 }
+
--- project3.h
+++ project3.h
@@ -3,3 +3,4 @@
 //Project #3
 //
 
+void shiftleft2x(unsigned char out[32], unsigned char in[32]);
