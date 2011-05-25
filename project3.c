eb381b11315cbd8b6f2ae06b636598e1342bebb9
Erik Steggall <esteggal@ucsc.edu>
2011-05-25 01:06:24 -0700

    tested shiftleft2x

--- project3.c
+++ project3.c
@@ -6,8 +6,8 @@
 
 void shiftleft2x(unsigned char out[32], unsigned char in[32]){
   int i;
-  out[0] = 0;
-  out[1] = 0;
+  out[0] = '0';
+  out[1] = '0';
   for(i = 1;i < 31;i++){
     out[i+1] = in[i];
   }
--- test.c
+++ test.c
@@ -2,3 +2,27 @@
 //CMPE 110
 //Project #3
 //
+#include <stdio.h>
+#include <stdlib.h>
+#include "project1.h"
+#include "project2.h"
+#include "project3.h"
+
+int main(){
+  int itor;
+  unsigned char* in;
+  unsigned char* out;
+  in = malloc(32*sizeof(unsigned char) );
+  out = malloc(32*sizeof(unsigned char) );
+
+  for(itor = 0; itor < 31; itor++){
+    in[itor] = '1';
+    out[itor] = '0';
+  }
+
+  shiftleft2x(out, in);
+  printf("shift left\n");
+  print_bits(out);
+  return 0;
+
+}
